dfa9ac5c61c87b8398e432da042c26a896fab494
Jerry Janvier <37149800+jerrizzy@users.noreply.github.com>
2025-06-30 01:38:18 -0400

    Improve landing banner styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -26,6 +26,7 @@ function Home() {
   return (
     <div className="banner">
       <h1>Discover your favorite barber.</h1>
+      <h3>Find top-rated barbers near you.</h3>
       <form onSubmit={handleSearchSubmit} className="search">
           <input
             type="text"
@@ -34,19 +35,19 @@ function Home() {
             placeholder="Search for your barber..."
             className="input-text"
           />
-          <button kind="BUTTON/FLAT_SECONDARY" aria-label="Find Restaurants" type="button" class="styles__ButtonRoot-sc-1nqx07s-0 hje"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="styles__StyledInlineSvg-sc-1hetb2e-0 eCVqVv fetched-icon"><path d="M12.2929 17.2929C11.9024 17.6834 11.9024 18.3166 12.2929 18.7071C12.6834 19.0976 13.3166 19.0976 13.7071 18.7071L19.1578 13.2564C19.242 13.1722 19.3427 13.0717 19.4241 12.9758C19.5183 12.8648 19.6439 12.6962 19.7195 12.4635C19.8174 12.1623 19.8174 11.8377 19.7195 11.5365C19.6439 11.3038 19.5183 11.1352 19.4241 11.0242C19.3427 10.9283 19.242 10.8278 19.1578 10.7436L13.7071 5.29289C13.3166 4.90237 12.6834 4.90237 12.2929 5.29289C11.9024 5.68342 11.9024 6.31658 12.2929 6.70711L16.5858 11L5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44771 13 5 13L16.5858 13L12.2929 17.2929Z" fill="currentColor"></path></svg></button>
+          <button type="submit" className="search-button">Search</button>
         </form>
       <div className="search-container">
-        
-      </div> 
 
-    <div>    
-      <h3>Real people, real reviews.</h3>
-      </div>  
-      
       </div>
 
-      
+    <div>
+      <h3>Real people, real reviews.</h3>
+      </div>
+
+      </div>
+
+
   )
 }
 
